8360b834b0da28696ce8c627178c5837fc91735b
Pascal Heeraman <pascal@tapendium.com>
2023-03-27 16:02:41 +1100

    Fix linting issues

--- lib/shared.sh
+++ lib/shared.sh
@@ -22,33 +22,6 @@ function readField {
 	echo "${result}"
 }
 
-# retry <number-of-retries> <jitter-factor> <command>
-function retry {
-	local maxAttempts=$1
-	shift
-	local jitterFactor=$1
-	shift
-	local attempt=1
-
-	while true; do
-		"$@" && break || {
-			if [[ $attempt -lt $maxAttempts ]]; then
-				backOff=$(echo "1*1.5^$attempt" | bc)
-				maxJitter=$(echo "$jitterFactor * $backOff" | bc)
-				minJitter=$(echo "$maxJitter * -1" | bc)
-				jitter=$(echo "scale=2; ($RANDOM / 32768) * ($maxJitter - $minJitter) + $minJitter" | bc)
-				delay=$(echo "$backOff + $jitter" | bc)
-				echo "Attempt $attempt/$maxAttempts failed. Retrying in ${delay}s"
-				((attempt++))
-				sleep "$delay"
-			else
-				echo 1>&2 "The command has failed after $maxAttempts attempts."
-				exit 1
-			fi
-		}
-	done
-}
-
 # Fetch API token from Secret Manager.
 #
 # Assumes token is stored as plaintext
